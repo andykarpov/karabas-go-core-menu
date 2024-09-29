d997086d9f19642270b4af931037b873ea177fe7
Andy Karpov <andykarpov@users.noreply.github.com>
2024-09-30 00:35:20 +0300

    Refactored DVI output for FT

--- src/rtl/karabas_mini_top.vhd
+++ src/rtl/karabas_mini_top.vhd
@@ -31,6 +31,7 @@ use unisim.vcomponents.all;
 
 entity karabas_mini is
     Port ( CLK_50MHZ : in  STD_LOGIC;
+			  CLK_50MHZ_OLD : in STD_LOGIC;
 
            TAPE_IN  : in  STD_LOGIC;
            TAPE_OUT : out  STD_LOGIC;
@@ -103,7 +104,7 @@ entity karabas_mini is
            MCU_SCK : in  STD_LOGIC;
            MCU_MOSI : in  STD_LOGIC;
            MCU_MISO : out  STD_LOGIC;
-           MCU_IO : inout STD_LOGIC_VECTOR(4 downto 0)
+           MCU_IO : inout STD_LOGIC_VECTOR(3 downto 0)
 );
 end karabas_mini;
 
@@ -118,13 +119,25 @@ signal shift		: std_logic_vector(7 downto 0);
 signal red		: std_logic_vector(7 downto 0);
 signal green		: std_logic_vector(7 downto 0);
 signal blue		: std_logic_vector(7 downto 0);
-signal clk_vga		: std_logic;
 signal locked, lockedx5 : std_logic;
 signal areset 		: std_logic;
-signal v_clk_vga : std_logic;
+
+signal sysclk, sysclk_buf   : std_logic;
+signal clk_vga, clk_vga_buf : std_logic;
+signal clkpll1_fbout, clkpll1_fbout_buf : std_logic;
+signal clkfbout : std_logic;
+signal pllclk0, pllclk1, pllclk2 : std_logic;
+signal pll_lckd, pclk_lckd : std_logic;
+signal pclk, pclkx2, pclkx10 : std_logic;
+signal serdesstrobe : std_logic;
+signal bufpll_lock: std_logic;
+signal serdes_rst : std_logic := '0';
+signal tmdsclkint : std_logic_vector(4 downto 0) := "00000";
+signal tmdsclk : std_logic;
+signal toggle : std_logic := '0';
+signal tmdsint : std_logic_vector(2 downto 0);
+
 signal v_clk_int : std_logic;
-signal clk_hdmi : std_logic;
-signal clk_hdmi_n : std_logic;
 
 signal osd_rgb : std_logic_vector(23 downto 0);
 signal osd_command: std_logic_vector(15 downto 0);
@@ -136,13 +149,21 @@ signal ft_sck    : std_logic := '1';
 signal ft_mosi   : std_logic := '1';
 signal ft_audio_stb : std_logic := '0';
 signal ft_audio_data : std_logic_vector(11 downto 0);
+signal ft_clk_ibuf, ft_clk_buf : std_logic;
 
 signal host_vga_r, host_vga_g, host_vga_b : std_logic_vector(7 downto 0);
 signal host_vga_hs, host_vga_vs, host_vga_blank : std_logic;
-signal tmds_red, tmds_green, tmds_blue : std_logic_vector(9 downto 0);
+signal tmds_red, tmds_green, tmds_blue : std_logic_vector(4 downto 0);
 
 signal adc_l, adc_r : std_logic_vector(23 downto 0);
 signal audio_mix_l, audio_mix_r : std_logic_vector(15 downto 0);
+
+signal prev_ft_vga_on, reset_pll2 : std_logic := '0';
+
+-- regs in ft_clk_buf clock 
+signal vga_r_r, vga_g_r, vga_b_r, vga_r_r2, vga_g_r2, vga_b_r2 : std_logic_vector(7 downto 0);
+signal vga_hs_r, vga_vs_r, vga_hs_r2, vga_vs_r2 : std_logic;
+signal ft_de_r, ft_de_r2 : std_logic;
 
 begin
 
@@ -175,28 +196,95 @@ MIDI_CLK <= '0';
 MIDI_RST_N <= '1';
 FT_RESET <= '1';
 
--- PLL
-pll0_inst: entity work.pll 
+-- GLobal clock
+
+sysclk_ibuf: IBUF port map(I => CLK_50MHZ, O => sysclk);
+sysclk_bufg: BUFG port map(I => sysclk, O => sysclk_buf);
+
+-- 25 MHz PLL
+pll1: PLL_BASE
+generic map(
+	BANDWIDTH => "OPTIMIZED",
+	CLK_FEEDBACK => "CLKFBOUT",
+	COMPENSATION => "SYSTEM_SYNCHRONOUS",
+	DIVCLK_DIVIDE => 1,
+	CLKFBOUT_MULT => 8,
+	CLKFBOUT_PHASE => 0.000,
+	CLKOUT0_DIVIDE => 16,
+	CLKOUT0_PHASE => 0.000,
+	CLKOUT0_DUTY_CYCLE => 0.500,
+	CLKIN_PERIOD => 20.0,
+	REF_JITTER => 0.010
+)
 port map(
-	CLK_IN1 => CLK_50MHZ,
-	CLK_OUT1 => clk_vga,
-	LOCKED => locked
+	CLKIN => sysclk,
+	RST => '0',
+
+	CLKOUT0 => clk_vga,
+	LOCKED => locked,
+	
+	CLKFBIN => clkpll1_fbout_buf,
+	CLKFBOUT => clkpll1_fbout
 );
 
-pllx5_inst : entity work.pllx5
-port map(
-	CLK_IN1 => v_clk_int,
-	CLK_OUT1 => clk_hdmi,
-	CLK_OUT2 => clk_hdmi_n,
-	LOCKED => lockedx5
-);
-
+sysclkf_buf0: BUFG port map(O => clkpll1_fbout_buf, I => clkpll1_fbout);
+clk_vga_buf0: BUFG port map(O => clk_vga_buf, I => clk_vga);
 areset <= not locked;
+
+-- PLL 1x, 2x, 10x 
+
+pll2: PLL_BASE 
+generic map(
+	CLKIN_PERIOD => 13.0,
+	CLKFBOUT_MULT => 10, -- set VCO to 10x of CLKIN
+	CLKOUT0_DIVIDE => 1,
+	CLKOUT1_DIVIDE => 10,
+	CLKOUT2_DIVIDE => 5,
+	COMPENSATION => "INTERNAL"	
+)
+port map (
+	CLKFBOUT => clkfbout,
+	CLKOUT0 => pllclk0,
+	CLKOUT1 => pllclk1,
+	CLKOUT2 => pllclk2,
+	LOCKED => pll_lckd,
+	CLKFBIN => clkfbout,
+	CLKIN => v_clk_int,
+	RST => reset_pll2 -- not(pclk_lckd)
+  );
+  
+  pclk_lckd <= locked;
+  
+  process(clk_vga_buf) 
+  begin
+	if rising_edge(clk_vga_buf) then
+		reset_pll2 <= '0';
+		if prev_ft_vga_on /= ft_vga_on then
+			reset_pll2 <= '1';
+			prev_ft_vga_on <= ft_vga_on;
+		end if;
+	end if;
+  end process;
+  
+bufpll0: BUFPLL generic map(
+	DIVIDE => 5
+) 
+port map (
+	PLLIN => pllclk0, 
+	GCLK => pclkx2, 
+	LOCKED => pll_lckd, 
+	IOCLK => pclkx10, 
+	SERDESSTROBE => serdesstrobe, 
+	LOCK => bufpll_lock
+);
+
+pclkbufg: BUFG port map (I => pllclk1, O => pclk);
+pclkx2bufg: BUFG port map (I => pllclk2, O => pclkx2);
 
 -- VGA SYNC
 vga_sync_inst: entity work.vga_sync
 port map(
-	CLK => clk_vga,
+	CLK => clk_vga_buf,
 	HSYNC => hsync,
 	VSYNC => vsync,
 	BLANK => blank,
@@ -208,7 +296,7 @@ port map(
 -- OSD
 osd_inst: entity work.overlay
 port map(
-	CLK => clk_vga,
+	CLK => clk_vga_buf,
 	RGB_I => red & green & blue,
 	RGB_O => osd_rgb,
 	HCNT_I => hcnt,
@@ -219,7 +307,7 @@ port map(
 -- MCU
 mcu_inst: entity work.mcu
 port map(
-	CLK => clk_vga,
+	CLK => clk_vga_buf,
 	N_RESET => not areset,
 	
 	MCU_SPI_MOSI => MCU_MOSI,
@@ -248,56 +336,141 @@ red	<= (hcnt(7 downto 0) + shift) and "11111111";
 green	<= (vcnt(7 downto 0) + shift) and "11111111";
 blue	<= (hcnt(7 downto 0) + vcnt(7 downto 0) - shift) and "11111111";
 
-host_vga_r <= VGA_R when ft_vga_on = '1' else osd_rgb(23 downto 16) when blank = '0' else "00000000";
-host_vga_g <= VGA_G when ft_vga_on = '1' else osd_rgb(15 downto 8) when blank = '0' else "00000000";
-host_vga_b <= VGA_B when ft_vga_on = '1' else osd_rgb(7 downto 0) when blank = '0' else "00000000";
-host_vga_hs <= VGA_HS when ft_vga_on = '1' else hsync;
-host_vga_vs <= VGA_VS when ft_vga_on = '1' else vsync;
-host_vga_blank <= not(FT_DE) when ft_vga_on = '1' else blank;
+process (pclk)
+begin
+	if rising_edge(pclk) then
+		vga_r_r <= VGA_R;
+		vga_g_r <= VGA_G;
+		vga_b_r <= VGA_B;
+		vga_hs_r <= VGA_HS;
+		vga_vs_r <= VGA_VS;
+		ft_de_r <= FT_DE;
+		vga_r_r2 <= vga_r_r;
+		vga_g_r2 <= vga_g_r;
+		vga_b_r2 <= vga_b_r;
+		vga_hs_r2 <= vga_hs_r;
+		vga_vs_r2 <= vga_vs_r;
+		ft_de_r2 <= ft_de_r;
+	end if;
+end process;
+
+host_vga_r <= vga_r_r2 when ft_vga_on = '1' else osd_rgb(23 downto 16) when blank = '0' else "00000000";
+host_vga_g <= vga_g_r2 when ft_vga_on = '1' else osd_rgb(15 downto 8) when blank = '0' else "00000000";
+host_vga_b <= vga_b_r2 when ft_vga_on = '1' else osd_rgb(7 downto 0) when blank = '0' else "00000000";
+host_vga_hs <= vga_hs_r2 when ft_vga_on = '1' else hsync;
+host_vga_vs <= vga_vs_r2 when ft_vga_on = '1' else vsync;
+host_vga_blank <= not(ft_de_r2) when ft_vga_on = '1' else blank;
+
+FT_CLK_IBUF0: IBUF
+port map (
+	I		=> FT_CLK,
+	O		=> ft_clk_ibuf
+);
+
+FT_CLK_BUF0: BUFG
+port map (
+	I		=> ft_clk_ibuf,
+	O 		=> ft_clk_buf
+);
 
 V_CLK_MUX : BUFGMUX_1
 port map (
- I0      => clk_vga,
- I1      => FT_CLK,
+ I0      => clk_vga_buf,
+ I1      => ft_clk_buf,
  O       => v_clk_int,
  S       => ft_vga_on
 );
 
 -- TODO: HDMI
-hdmi: entity work.hdmi
-generic map(
-	FREQ => 25000000, -- TODO: remap as signals to adjust on the fly ?
-	FS => 48000,
-	CTS => 25000,
-	N => 6144
-)
+
+-- todo: replace to hdmi with sound
+enc0 : entity work.dvi_encoder
+port map (
+    clkin => pclk,
+    clkx2in => pclkx2,
+    rstin => areset,
+    blue_din => host_vga_b,
+    green_din => host_vga_g,
+    red_din => host_vga_r,
+    hsync => host_vga_hs,
+    vsync => host_vga_vs,
+    de => not host_vga_blank,
+    tmds_data0 => tmds_red,
+    tmds_data1 => tmds_green,
+    tmds_data2 => tmds_blue);
+	 
+-- DVI serializers and OB
+	 
+oserdes_0: entity work.serdes_n_to_1
+generic map (SF => 5)
 port map(
-	I_CLK_PIXEL => v_clk_int,
-	I_R => host_vga_r,
-	I_G => host_vga_g,
-	I_B => host_vga_b,
-	I_BLANK => host_vga_blank, -- todo
-	I_HSYNC => host_vga_hs,
-	I_VSYNC => host_vga_vs,
-	I_AUDIO_ENABLE => '1', -- todo
-	I_AUDIO_PCM_L => audio_mix_l,
-	I_AUDIO_PCM_R => audio_mix_r,
-	O_RED => tmds_red,
-	O_GREEN => tmds_green,
-	O_BLUE => tmds_blue
+	ioclk => pclkx10,
+	serdesstrobe => serdesstrobe,
+	reset => serdes_rst,
+	gclk => pclkx2,
+	datain => tmds_red,
+	iob_data_out => tmdsint(0)
 );
 
-hdmio: entity work.hdmi_out_xilinx
+oserdes_1: entity work.serdes_n_to_1
+generic map (SF => 5)
 port map(
-	clock_pixel_i => v_clk_int,
-	clock_tdms_i => clk_hdmi,
-	clock_tdms_n_i => clk_hdmi_n,
-	red_i => tmds_red,
-	green_i => tmds_green,
-	blue_i => tmds_blue,
-	tmds_out_p => tmds_p,
-	tmds_out_n => tmds_n	
+	ioclk => pclkx10,
+	serdesstrobe => serdesstrobe,
+	reset => serdes_rst,
+	gclk => pclkx2,
+	datain => tmds_green,
+	iob_data_out => tmdsint(1)
 );
+
+oserdes_2: entity work.serdes_n_to_1
+generic map (SF => 5)
+port map(
+	ioclk => pclkx10,
+	serdesstrobe => serdesstrobe,
+	reset => serdes_rst,
+	gclk => pclkx2,
+	datain => tmds_blue,
+	iob_data_out => tmdsint(2)
+);
+
+oserdes_3: entity work.serdes_n_to_1
+generic map (SF => 5)
+port map(
+	ioclk => pclkx10,
+	serdesstrobe => serdesstrobe,
+	reset => serdes_rst,
+	gclk => pclkx2,
+	datain => tmdsclkint,
+	iob_data_out => tmdsclk
+);
+
+TMDS0: OBUFDS port map (I => tmdsint(0), O => TMDS_P(0), OB => TMDS_N(0));
+TMDS1: OBUFDS port map (I => tmdsint(1), O => TMDS_P(1), OB => TMDS_N(1));
+TMDS2: OBUFDS port map (I => tmdsint(2), O => TMDS_P(2), OB => TMDS_N(2));
+TMDS3: OBUFDS port map (I => tmdsclk, O => TMDS_P(3), OB => TMDS_N(3));
+
+process (pclkx2, serdes_rst)
+begin
+	if serdes_rst = '1' then 
+		toggle <= '0';
+	elsif rising_edge(pclkx2) then
+		toggle <= not toggle;
+	end if;
+end process;
+
+process (pclkx2)
+begin
+	if rising_edge(pclkx2) then
+		if toggle = '1' then
+			tmdsclkint <= "11111";
+		else 
+			tmdsclkint <= "00000";
+		end if;
+	end if;
+end process;
+
+serdes_rst <= areset;
 
 -- Sigma-Delta DAC
 dac_l : entity work.dac
